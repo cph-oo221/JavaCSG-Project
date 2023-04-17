a8a607c595bcd24ef3ddf46c174b572b545578a0
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-04-17 23:12:36 +0200

    update

--- src/main/java/Car/CarBody.java
+++ src/main/java/Car/CarBody.java
@@ -1,5 +1,27 @@
 package Car;
 
+import org.abstractica.javacsg.Geometry3D;
+import org.abstractica.javacsg.JavaCSG;
+
 public class CarBody
 {
+    private final JavaCSG csg;
+    private final double width;
+    private final double height;
+    private final double length;
+
+    public CarBody(JavaCSG csg, double width, double height, double length)
+    {
+        this.csg = csg;
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public Geometry3D generate()
+    {
+        Geometry3D body = csg.box3D(width, height, length, true);
+        Geometry3D centerHole = csg.box3D(width / 1.2, height / 1.2, length / 1.2, true);
+        return csg.difference3D(body, centerHole);
+    }
 }
--- src/main/java/Car/CarBodyTest.java
+++ src/main/java/Car/CarBodyTest.java
@@ -1,5 +1,14 @@
 package Car;
 
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
+
 public class CarBodyTest
 {
+    public static void main(String[] args)
+    {
+        JavaCSG csg = JavaCSGFactory.createDefault();
+        CarBody body = new CarBody(csg, 50, 20, 100);
+        csg.view(body.generate());
+    }
 }
