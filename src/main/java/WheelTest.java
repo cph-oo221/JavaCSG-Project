d0d9306f104af4b6b9d48a853d0df2a739154380
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-04-17 15:51:20 +0200

    update

---
+++ src/main/java/WheelTest.java
@@ -0,0 +1,13 @@
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
+
+public class WheelTest
+{
+    public static void main(String[] args)
+    {
+        JavaCSG csg = JavaCSGFactory.createDefault();
+        Wheel wheel = new Wheel(csg, 20, 5, 360, 20,0, 10);
+        
+        csg.view(wheel.generate());
+    }
+}
